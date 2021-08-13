093ec071c8345b52e785d3f5006bd13afe300a00
baban9 <babandeep193@gmail.com>
2021-08-13 22:59:01 +0530

    updated utils and ethnicity notebook

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,13 @@
 from sklearn import preprocessing, metrics
 from sklearn.model_selection import train_test_split
+
 import glob
 import pandas as pd
 import numpy as np
 
+from sklearn.linear_model import LogisticRegression
+from keras.models import Sequential
+from keras.layers import Dense
 
 def get_sampled_data(dataset_path, n = 1000):
   
@@ -14,15 +18,15 @@ def get_sampled_data(dataset_path, n = 1000):
   else:
     return data
 
-def temp():
-  #TODO
-  for folder in tqdm(folders):
-  csv_files = os.listdir(DATA_DIR + folder)
-  for csv in csv_files:
-    if '.csv' in csv:
-      if DEBUG: print(DATA_DIR + "full/" + folder + "/" + csv)
-      data_sample = get_sampled_data(DATA_DIR + folder + "/" + csv)
-      data_sample.to_csv(DATA_DIR + "sample/" + folder + csv)
+# def temp():
+#   #TODO
+#   for folder in tqdm(folders):
+#   csv_files = os.listdir(DATA_DIR + folder)
+#   for csv in csv_files:
+#     if '.csv' in csv:
+#       if DEBUG: print(DATA_DIR + "full/" + folder + "/" + csv)
+#       data_sample = get_sampled_data(DATA_DIR + folder + "/" + csv)
+#       data_sample.to_csv(DATA_DIR + "sample/" + folder + csv)
 
 
 def get_clean_data(dataset_path, drop_col):
@@ -44,23 +48,60 @@ def get_clean_data(dataset_path, drop_col):
   return data
 
 
-def fit_and_get_metrics(data, model):
 
-  assert 'pol' in data.columns
+def get_model(dimension_input):
+  model = Sequential()
+  model.add(Dense(1024, input_dim=dimension_input, activation='relu'))
+  model.add(Dense(512, input_dim=1024, activation='relu'))
+  model.add(Dense(256, input_dim=512, activation='relu'))
+  model.add(Dense(128, input_dim=256, activation='relu'))
+  model.add(Dense(60, input_dim=128, activation='relu'))
+  model.add(Dense(1, activation='sigmoid'))
+  # Compile model
+  model.compile(loss='binary_crossentropy', optimizer='SGD', metrics=['accuracy'])
 
-  y = data['pol']
+  return model
+
+def fit_and_get_metrics(data, model_name):
+
+  y = data['pol'].replace({"liberal":1,"conservative":0})
   X = data.drop('pol', axis = 1)
 
   X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2) 
 
-  model.fit(X_train, y_train)
+  if model_name == "LR":
+    # assert 'pol' in data.columns
+    lr = LogisticRegression(penalty='l1',solver="saga")
 
-  y_pred = model.predict_proba(X_test)
+    lr.fit(X_train, y_train)
 
-  auc = round(metrics.roc_auc_score(y_test,y_pred[:,1]),2)*100
-  acc = round(metrics.accuracy_score(y_test, model.predict(X_test)),2)*100
+    y_pred = lr.predict_proba(X_test)
 
-  return auc, acc, data.shape[0]
+    auc = round(metrics.roc_auc_score(y_test,y_pred[:,1])*100,2)
+    acc = round(metrics.accuracy_score(y_test, lr.predict(X_test))*100,2)
+
+    return auc, acc
+  
+  elif model_name == "NN":
+    # assert 'pol' in data.columns
+
+    model = get_model(X_train.shape[1])
+    model.fit(epochs=25,x=X_train,y=y_train,batch_size=1000, verbose=0, validation_split=0.2)
+
+    y_pred = model.predict(X_test)
+  
+    auc = round(metrics.roc_auc_score(y_test,y_pred)*100,2)
+
+    _, acc = model.evaluate(X_test, y_test,batch_size=1000, verbose=0)
+
+    return auc, round(acc*100, 2)
+  
+  else:
+    print(" Enter Model name LR or NN for Logistic regression or Neural Network respectively.")
+
+  
+def get_dataframe_name(dataset_path):
+  return "_".join(dataset_path.split("/")[-1:][0][:-4].split("_")[1:])
 
 #--------------------------------------------- raw
 
@@ -89,7 +130,8 @@ def get_dataframe(segment_to_run = "Canada_0_dating"):
 
   return pd.concat(li, axis=0, ignore_index=True)
 
-
+def get_experiment_data(dataframe, column_list):
+  return dataframe[column_list]
 
 def save_eda(arr):
   # saving the results 
